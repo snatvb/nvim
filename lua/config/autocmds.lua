b048c137e12993e030eb6107de8f0a90a59cbb44
snatvb <snatvb@ya.ru>
2025-01-15 22:35:57 +0200

    Added more LSP; Autofix stylelint; Surround plugin

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,17 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+--
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = { "*.css", "*.scss", "*.sass" },
+  callback = function()
+    local filepath = vim.fn.expand("%:p")
+    if vim.fn.executable("stylelint") == 1 or vim.fn.filereadable("./node_modules/.bin/stylelint") == 1 then
+      local cmd = "npx stylelint --fix " .. filepath
+      vim.fn.system(cmd)
+      vim.api.nvim_command("edit!")
+    else
+      vim.notify("stylelint is not installed!", vim.log.levels.WARN)
+    end
+  end,
+})
